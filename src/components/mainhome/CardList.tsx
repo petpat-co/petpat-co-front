6f55aa49527792bdd9b587d3e0bdd07996ced819
Kopite93 <1000nulee@naver.com>
2022-11-04 00:23:28 +0900

    [refactor] CardList component refactoring

--- src/components/mainhome/CardList.tsx
+++ src/components/mainhome/CardList.tsx
@@ -1,16 +1,22 @@
-import React from "react";
 import Card from "../shared/Card";
-import * as S from "./Container.style";
+import * as S from "./CardList.styled";
+import { ReactComponent as Arrow } from "../../asset/arrowIcon.svg";
 
 const CardList = () => {
   const test: number[] = [1, 2, 3, 4];
 
   return (
-    <S.CardBox>
-      {test.map((el, idx) => (
-        <Card key={idx} />
-      ))}
-    </S.CardBox>
+    <>
+      <S.CardBox height="40px" margin="0 auto 40px auto">
+        <S.Text>텍스트들어갈자리</S.Text>
+        <Arrow />
+      </S.CardBox>
+      <S.CardBox height="420px">
+        {test.map((el, idx) => (
+          <Card key={idx} />
+        ))}
+      </S.CardBox>
+    </>
   );
 };
 

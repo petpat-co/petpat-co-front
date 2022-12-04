f959ed2b75d438510eb2188c13912e26eacadbb0
edel <dbfla5036@naver.com>
2022-12-04 21:51:55 +0900

    feat: #2 api instance 수정

--- src/network/instance.ts
+++ src/network/instance.ts
@@ -1,31 +1,48 @@
-import axios from "axios";
-import { config } from "./config";
+import axios from 'axios';
+import { config } from './config';
 
 const instance = axios.create({
   baseURL: config.server.host,
   timeout: 20000,
   withCredentials: true,
   headers: {
-    "content-type": "application/json",
+    'content-type': 'application/json; charset=UTF-8',
+    accept: 'application/json',
   },
 });
 
+// instance.interceptors.request.use(
+//   function (config) {
+//     return config;
+//   },
+//   function (error) {
+//     return Promise.reject(error);
+//   },
+// );
+//2. 요청 인터셉터
 instance.interceptors.request.use(
-  function (config) {
+  //요청직전 호출
+  (config) => {
+    const Token = localStorage.getItem('token');
+    config.headers = {
+      'content-type': 'application/json;charset=UTF-8',
+      accept: 'application/json',
+      Authorization: `Bearer ${Token}`,
+    };
     return config;
   },
-  function (error) {
-    return Promise.reject(error);
-  }
+  //에러 전 호출
+  (err) => {
+    return Promise.reject(err);
+  },
 );
-
 instance.interceptors.response.use(
   function (response) {
     return response;
   },
   function (error) {
     return Promise.reject(error);
-  }
+  },
 );
 
 export default instance;

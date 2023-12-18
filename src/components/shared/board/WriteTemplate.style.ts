1480d811bcf62235089c04b5c818bb51bc140edd
JISOON-Y <zsooon96@gmail.com>
2023-12-19 08:34:02 +0900

    feat: 물품판매 작성화면 퍼블리싱 진행중 - 작성화면 공통 스타일 및 레이아웃 작업

---
+++ src/components/shared/board/WriteTemplate.style.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import theme from '../../../styles/theme';
+
+// 게시판 작성화면 공통 스타일
+export const InputSectionContainer = styled.div`
+  display: flex;
+  padding: 4%;
+`;
+
+export const InputTitleWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  gap: 6px;
+  padding-top: 0.3%;
+`;
+
+export const InputTitleText = styled.h2`
+  font-weight: ${theme.fontWeights.regular};
+  font-size: ${theme.fontSizes.titleSize};
+`;
+
+export const InputSectionWrapper = styled.div`
+  flex: 4;
+`;
+
+export const RowContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const ColumnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const InputSubText = styled.p`
+  font-size: ${theme.fontSizes.xlg};
+  color: ${theme.colors.primary};
+`;
+
+export const DividerLine = styled.div`
+  flex: 1;
+  height: 1px;
+  background: ${theme.colors.borderColor};
+`;
